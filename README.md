05ba271d84e05483e16496285eb97dfb3ccd1800
Victor Genty <vgenty@nevis.columbia.edu>
2016-07-12 16:02:54 -0400

    specify number of events

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Python tool like unix csplit but which splits binary files based on hexadecimal 
 
 Example usage:
 
-$ csplitb.py --n 5 ffffffffffff binary.file
+$ csplitb.py -e 5 -n 5 ffffffffffff binary.file
+
+5 events, split into 5 files, split on 48 bits, binary file input
--- csplitb.py
+++ csplitb.py
@@ -8,10 +8,9 @@ import binascii
 #Memory-mapped file objects behave like both strings and like file objects. Unlike normal string objects, however, these are mutable. You can use mmap objects in most places where strings are expected; for example, you can use the re module to search through a memory-mapped file. 
 import mmap
 
-
 class CSplitB(object):
 
-    def __init__(self, spliton, infile, number, prefix, suffix):
+    def __init__(self, spliton, infile, number, prefix, suffix, events):
         #Return the binary data represented by the hexadecimal string hexstr. This function is the inverse of b2a_hex(). hexstr must contain an even number of hexadecimal digits (which can be upper or lower case), otherwise a TypeError is raised.
         self.spliton_str = binascii.unhexlify(spliton)
 
@@ -19,13 +18,16 @@ class CSplitB(object):
             prefix = "xx"
         if not suffix:
             suffix = ".dat"
-
+        if not events:
+            events = -1
+        
         self.infile = infile
         self.number = number
         self.prefix = prefix
         self.suffix = suffix
+
+        self.events = events
         self.number_fmt = "%%0%dd" % self.number
-        print "Self.number is {}".format(self.number_fmt)
         self.last_idx = -1
         self.count = 0
 
@@ -34,7 +36,6 @@ class CSplitB(object):
             self.mm = mmap.mmap(f.fileno(), 0)
             while True:
                 idx = self.mm.find(self.spliton_str, self.last_idx + 1)
-                print "idx is ",idx," self.last_idx is ",self.last_idx
                 if idx == -1:
                     self.finish()
                     break
@@ -54,19 +55,24 @@ class CSplitB(object):
         outfile = self.prefix + (self.number_fmt % self.count) + self.suffix
         with open(outfile, "w+b") as f:
             f.write(data)
+
         self.count += 1
 
+        if self.events < 0: return
+        if self.count >= self.events: sys.exit(1)
 
 def main(argv = sys.argv):
+
     parser = argparse.ArgumentParser(description="csplitb - Context splitter on binary data.")
     parser.add_argument("spliton", help="Hexadecimal representation of data to split on.")
     parser.add_argument("infile", help="Input file.")
     parser.add_argument("-n", "--number", type=int, help="Number of zeroes to pad filename. No default")
     parser.add_argument("-f", "--prefix", help="Output file prefix. Default is xx")
     parser.add_argument("-s", "--suffix", help="Output file suffix. Default is .dat")
+    parser.add_argument("-e", "--events", type=int, help="Number of events you want. Default is -1")
     args = parser.parse_args(argv[1:])
 
-    csplitb = CSplitB(args.spliton, args.infile, args.number, args.prefix, args.suffix)
+    csplitb = CSplitB(args.spliton, args.infile, args.number, args.prefix, args.suffix, args.events)
     return csplitb.run()
 
 if __name__ == '__main__':
